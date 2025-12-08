18dde9ae7beb6518596fc1e3514fcc7d78faae13
Spit365 <lukas.grossauer47@gmail.com>
2025-12-08 12:48:49 +0100

    Modified Blood Particle Behaviour

--- src/client/java/net/spit365/clienttweaks/particle/BloodParticle.java
+++ src/client/java/net/spit365/clienttweaks/particle/BloodParticle.java
@@ -14,6 +14,8 @@ import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class BloodParticle extends SpriteBillboardParticle {
      private static final LinkedList<BloodParticle> bloodParticles = new LinkedList<>();
@@ -21,8 +23,8 @@ public class BloodParticle extends SpriteBillboardParticle {
      protected BloodParticle(ClientWorld world, double x, double y, double z, SpriteProvider spriteProvider) {
           super(world, x, y, z, 0, 0, 0);
           this.setSprite(spriteProvider.getSprite(world.random));
-          this.velocityMultiplier = Math.min(world.random.nextFloat() * bloodParticles.size() / 2, 2) + 2f;
-          this.gravityStrength = 10f;
+          this.velocityMultiplier = 2f - 1f / bloodParticles.size();
+          this.gravityStrength = 5f;
           this.maxAge = 36000;
           this.scale = 1f;
           bloodParticles.add(this);
@@ -42,34 +44,41 @@ public class BloodParticle extends SpriteBillboardParticle {
 
      private double roundPixels(double i) {return ((int) (i * 16)) / 16d;}
 
-     @Override
-     public void render(VertexConsumer vertexConsumer, Camera camera, float tickProgress) {
+	@Override
+	public void render(VertexConsumer vertexConsumer, Camera camera, float tickProgress) {
 		float halfSize = this.getSize(tickProgress) / 2.0f;
 
-          float minU = this.getMinU();
-          float maxU = this.getMaxU();
-          float minV = this.getMinV();
-          float maxV = this.getMaxV();
+		float minU = this.getMinU();
+		float maxU = this.getMaxU();
+		float minV = this.getMinV();
+		float maxV = this.getMaxV();
 
-          Vec3d camPos = camera.getPos();
+		Vec3d camPos = camera.getPos();
 
-		float px = (float)(MathHelper.lerp(tickProgress, lastX, x) - camPos.getX());
-          float py = (float)(MathHelper.lerp(tickProgress, lastY, y) - camPos.getY() + 0.001 * Math.toIntExact(bloodParticles.stream().filter(bloodParticle -> Math.abs(bloodParticle.age - this.age) <= 1).count() +1));
-          float pz = (float)(MathHelper.lerp(tickProgress, lastZ, z) - camPos.getZ());
+		float px = (float) (MathHelper.lerp(tickProgress, lastX, x) - camPos.getX());
+		Vec3d pos = new Vec3d(this.x, this.y, this.z);
+		List<BloodParticle> list = bloodParticles.stream().filter(isNearBlood(pos)).toList();
+		//System.out.println(list.size() +  " " + list.indexOf(this));
+		float py = (float) (MathHelper.lerp(tickProgress, lastY, y) - camPos.getY()  + 0.001 * (list.indexOf(this) + 1));
+		float pz = (float) (MathHelper.lerp(tickProgress, lastZ, z) - camPos.getZ());
 
-          int light = getBrightness(tickProgress);
+		int light = getBrightness(tickProgress);
 
-          vertexConsumer.vertex(px - halfSize, py, pz - halfSize).texture(minU, minV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
-          vertexConsumer.vertex(px - halfSize, py, pz + halfSize).texture(minU, maxV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
-          vertexConsumer.vertex(px + halfSize, py, pz + halfSize).texture(maxU, maxV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
-          vertexConsumer.vertex(px + halfSize, py, pz - halfSize).texture(maxU, minV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
-     }
+		vertexConsumer.vertex(px - halfSize, py, pz - halfSize).texture(minU, minV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
+		vertexConsumer.vertex(px - halfSize, py, pz + halfSize).texture(minU, maxV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
+		vertexConsumer.vertex(px + halfSize, py, pz + halfSize).texture(maxU, maxV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
+		vertexConsumer.vertex(px + halfSize, py, pz - halfSize).texture(maxU, minV).color(red, green, blue, alpha).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0, 1, 0);
+	}
 
 	public static @NotNull ParticleFactory<SimpleParticleType> getBloodParticleFactory(SpriteProvider spriteProvider) {
 		return (SimpleParticleType type, ClientWorld world, double x, double y, double z, double dx, double dy, double dz) -> new BloodParticle(world, x, y, z, spriteProvider);
 	}
 
      public static boolean isTouchingBlood(Vec3d pos){
-          return bloodParticles.stream().anyMatch(bloodParticle -> pos.squaredDistanceTo(bloodParticle.x, bloodParticle.y, bloodParticle.z) < 0.25);
+          return bloodParticles.stream().anyMatch(isNearBlood(pos));
      }
+
+	 private static Predicate<BloodParticle> isNearBlood(Vec3d pos){
+		 return bloodParticle -> pos.squaredDistanceTo(bloodParticle.x, bloodParticle.y, bloodParticle.z)  < 0.25;
+	 }
 }
