39f4ac7fe710174202b11f337e76569112ac7b01
Spit365 <lukas.grossauer47@gmail.com>
2026-02-21 14:16:16 +0100

    mixing -> mixin

--- src/client/java/net/spit365/clienttweaks/mixin/LivingEntityMixin.java
+++ src/client/java/net/spit365/clienttweaks/mixin/LivingEntityMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixing {
+public class LivingEntityMixin {
 	@Inject(method = "addDeathParticles", at = @At("HEAD"), cancellable = true)
 	public void addDeathParticles(CallbackInfo ci){
 		if (((LivingEntity) (Object) this) instanceof PlayerEntity player) {
