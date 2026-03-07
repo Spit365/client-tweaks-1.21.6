26a1b303c9fb0ace1c36d2ca7dbb898f1352eeb8
Spit365 <lukas.grossauer47@gmail.com>
2026-03-07 10:21:43 +0100

    Fixed registration of the packet handler, will also show a log message now

--- src/client/java/net/spit365/clienttweaks/mod/ClientPackets.java
+++ src/client/java/net/spit365/clienttweaks/mod/ClientPackets.java
@@ -1,13 +1,15 @@
 package net.spit365.clienttweaks.mod;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.impl.networking.client.ClientNetworkingImpl;
+import net.spit365.clienttweaks.ClientTweaks;
 import net.spit365.clienttweaks.packet.BoxStateS2CPacket;
 import net.spit365.clienttweaks.renderer.BoxOutlineRenderer;
 
 public class ClientPackets {
     public static void init(){
-        if (ClientNetworkingImpl.PLAY.getHandlers().containsKey(BoxStateS2CPacket.ID.id()))
+        try {
             ClientPlayNetworking.registerGlobalReceiver(BoxStateS2CPacket.ID, (boxRenderS2CPacket, context) -> BoxOutlineRenderer.setState(boxRenderS2CPacket.boxContexts()));
+            ClientTweaks.LOGGER.info("Server side box outline interface found!");
+        } catch (Exception ignored) {}
     }
 }
